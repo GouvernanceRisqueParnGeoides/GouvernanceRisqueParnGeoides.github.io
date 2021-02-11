0c57e5b23929b4f9017540d6422b3baf3d0f39a4
GouvernanceRisqueParnGeoides <78935500+GouvernanceRisqueParnGeoides@users.noreply.github.com>
2021-02-11 22:43:26 +0100

    Add files via upload

---
+++ data/Gouvernance_0.js
@@ -0,0 +1,8 @@
+var json_Gouvernance_0 = {
+"type": "FeatureCollection",
+"name": "Gouvernance_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -0.670545514728675, 0.277517990934214 ] } }
+]
+}
